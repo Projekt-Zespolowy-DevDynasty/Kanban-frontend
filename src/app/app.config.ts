6cc25dc07b9b4d59dcf6c5e21d028fdd4c164373
MichallBB <166235@student.uwm.edu.pl>
2024-03-08 18:06:13 +0100

    modified:   src/app/app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,9 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { CardsService } from './service/cards.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()]
+  providers: [provideRouter(routes), provideHttpClient(), HttpClient]
 };
