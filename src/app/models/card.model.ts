a4149c1eb811f8351b9e0aa276956741499b1804
MichallBB <166235@student.uwm.edu.pl>
2024-04-02 21:19:52 +0200

    task model

--- src/app/models/card.model.ts
+++ src/app/models/card.model.ts
@@ -1,7 +1,10 @@
+import { TaskComponent } from "../task/task.component";
+import { Task } from "./task.model";
+
 export interface Card {
   id: number;
   name: string;
   maxTasksLimit: number;
   position: number;
-  tasks: any[];
+  tasks: Task[];
 }
---
+++ src/app/models/task.model.ts
@@ -0,0 +1,8 @@
+import { User } from "./user.model";
+
+export interface Task {
+    id: number;
+    name: string;
+    users: User[];
+  }
+  
